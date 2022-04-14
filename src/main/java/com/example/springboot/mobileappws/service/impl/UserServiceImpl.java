12beacb86bdb48059fba3b1f636f756f03d86710
Aryan Upadhyay <aryan.offc@gmail.com>
2022-04-14 23:05:26 +0530

    created Utils.java file and created Utils.generateRandomUserId method also implemented it in UserServiceImpl.createUser method.

--- src/main/java/com/example/springboot/mobileappws/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/springboot/mobileappws/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.springboot.mobileappws.service.impl;
 import com.example.springboot.mobileappws.UserRepository;
 import com.example.springboot.mobileappws.io.entity.UserEntity;
 import com.example.springboot.mobileappws.service.UserService;
+import com.example.springboot.mobileappws.shared.Utils;
 import com.example.springboot.mobileappws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+    Utils utils = new Utils();
 
     @Override
     public UserDto createUser(UserDto user) throws RuntimeException {
@@ -26,7 +28,7 @@ public class UserServiceImpl implements UserService {
         BeanUtils.copyProperties(user, userEntity);
 
         // HardCoding some values for testing.
-        userEntity.setUserId("12345");
+        userEntity.setUserId(utils.generateRandomUserId(30));
         userEntity.setEncryptedPassword("test");
         // All other values can be sent through JSON payload.
 
