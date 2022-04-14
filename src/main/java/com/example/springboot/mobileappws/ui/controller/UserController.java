47da010eea205846420a05adba8222db093836b0
Aryan Upadhyay <aryan.offc@gmail.com>
2022-04-14 11:21:50 +0530

    created Dummy body of CRUD operations on User in UserController.java.

--- src/main/java/com/example/springboot/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/example/springboot/mobileappws/ui/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.example.springboot.mobileappws.ui.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     // Controls all operations that can be performed on the user.
 
+    @GetMapping // maps the get request to this function.
+    public String getUser() {
+        return "get User was Called";
+    }
+
+    @PostMapping // maps to http POST request.
+    public String createUser() {
+        return "create User was Called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user was Called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete user was Called";
+    }
 }
