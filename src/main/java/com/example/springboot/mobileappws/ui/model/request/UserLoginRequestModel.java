7f1e32742228f1a253ec5665a33a80f5edaa4e07
Aryan Upadhyay <aryan.offc@gmail.com>
2022-04-15 13:39:27 +0530

    created UserLoginRequestModel.java file for SignIn process.

---
+++ src/main/java/com/example/springboot/mobileappws/ui/model/request/UserLoginRequestModel.java
@@ -0,0 +1,11 @@
+package com.example.springboot.mobileappws.ui.model.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginRequestModel {
+    private String email;
+    private String password;
+}
