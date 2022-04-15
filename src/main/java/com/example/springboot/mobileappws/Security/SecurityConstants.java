10357feda3233b314f8add3b0a78dfa7bbd2468e
Aryan Upadhyay <aryan.offc@gmail.com>
2022-04-15 22:44:42 +0530

    created SecurityConstants.java file.

---
+++ src/main/java/com/example/springboot/mobileappws/Security/SecurityConstants.java
@@ -0,0 +1,10 @@
+package com.example.springboot.mobileappws.Security;
+
+public class SecurityConstants {
+    public static final long EXPIRATION_TIME = 864000000; // 10 DAY EXPIRATION TIME
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/users";
+    public static final String TOKEN_SECRET = "jf9i4vksj43ovo";
+}
+
